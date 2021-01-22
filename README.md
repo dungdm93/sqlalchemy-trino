4e5cf9335aeb85fc9b923829d8092e85facc0e39
Đặng Minh Dũng <dungdm93@live.com>
2021-01-22 11:45:41 +0700

    chores: update README and setup.py
    Signed-off-by: Đặng Minh Dũng <dungdm93@live.com>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-sqlalchemy_trino
+sqlalchemy-trino
 ================
+_[Trino](https://trino.io/) (f.k.a PrestoSQL) dialect for SQLAlchemy._
+
+The primary purpose of this is provide a dialect for Trino that can be used with [Apache Superset](https://superset.apache.org/).
+But other use-cases should works as well.
+
+## Installation
+The driver can either be installed through PyPi or from the source code.
+### Through Python Package Index
+```bash
+pip install sqlalchemy-trino
+```
+
+### Latest from Source Code
+```bash
+pip install git+https://github.com/dungdm93/sqlalchemy-trino
+```
+
+## Usage
+To connect from SQLAlchemy to Trino, use connection string (URL) following this pattern:
+```
+trino://<username>:<password>@<host>:<port>/catalog/[schema]
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlalchemy-trino",
+    version="0.1.0",
+    author="Dũng Đặng Minh",
+    author_email="dungdm93@live.com",
+    description="Trino dialect for SQLAlchemy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dungdm93/sqlalchemy-trino",
+    keywords=["sqlalchemy", "trino"],
+    packages=["sqlalchemy_trino"],
+    license="Apache 2.0",
+    platforms=["any"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "sqlalchemy~=1.3",
+        "trino~=0.305",
+    ],
+    entry_points={
+        "sqlalchemy.dialects": [
+            "trino = sqlalchemy_trino.dialect:TrinoDialect",
+        ]
+    },
+)
