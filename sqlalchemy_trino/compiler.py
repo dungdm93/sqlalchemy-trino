b795dd309f4ccfb4eff869d46b2690e18ee420c4
Đặng Minh Dũng <dungdm93@live.com>
2021-04-22 14:48:22 +0700

    feat: TrinoTypeCompiper datatypes

--- sqlalchemy_trino/compiler.py
+++ sqlalchemy_trino/compiler.py
@@ -85,7 +85,44 @@ class TrinoDDLCompiler(compiler.DDLCompiler):
 
 
 class TrinoTypeCompiler(compiler.GenericTypeCompiler):
-    pass
+    def visit_FLOAT(self, type_, **kw):
+        precision = type_.precision or 32
+        if 0 <= precision <= 32:
+            return self.visit_REAL(type_, **kw)
+        elif 32 < precision <= 64:
+            return self.visit_DOUBLE(type_, **kw)
+        else:
+            raise ValueError(f"type.precision={type_.precision} is invalid")
+
+    def visit_DOUBLE(self, type_, **kw):
+        return "DOUBLE"
+
+    def visit_NUMERIC(self, type_, **kw):
+        return self.visit_DECIMAL(type_, **kw)
+
+    def visit_NCHAR(self, type_, **kw):
+        return self.visit_CHAR(type_, **kw)
+
+    def visit_NVARCHAR(self, type_, **kw):
+        return self.visit_VARCHAR(type_, **kw)
+
+    def visit_TEXT(self, type_, **kw):
+        return self.visit_VARCHAR(type_, **kw)
+
+    def visit_BINARY(self, type_, **kw):
+        return self.visit_VARBINARY(type_, **kw)
+
+    def visit_CLOB(self, type_, **kw):
+        return self.visit_VARCHAR(type_, **kw)
+
+    def visit_NCLOB(self, type_, **kw):
+        return self.visit_VARCHAR(type_, **kw)
+
+    def visit_BLOB(self, type_, **kw):
+        return self.visit_VARBINARY(type_, **kw)
+
+    def visit_DATETIME(self, type_, **kw):
+        return self.visit_TIMESTAMP(type_, **kw)
 
 
 class TrinoIdentifierPreparer(compiler.IdentifierPreparer):
