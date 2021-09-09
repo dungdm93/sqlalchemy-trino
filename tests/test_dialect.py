413be0efe395e47c54cefdf82e94e7fad04f9a4a
Ke Zhu <kzhu@us.ibm.com>
2021-09-08 20:46:22 -0400

    Support query string param accessToken

--- tests/test_dialect.py
+++ tests/test_dialect.py
@@ -1,5 +1,5 @@
 from sqlalchemy.engine import url
-from sqlalchemy_trino.dialect import TrinoDialect
+from sqlalchemy_trino.dialect import HTTPS, JWTAuthentication, TrinoDialect
 
 
 def test_trino_connection_string_user():
@@ -19,3 +19,14 @@ def test_trino_connection_string_session_user():
     _, cparams = dialect.create_connect_args(u)
 
     assert cparams['user'] == session_user
+
+
+def test_trino_connection_jwt_token():
+    dialect = TrinoDialect()
+    access_token = 'mock-token'
+    u = url.make_url(f'trino://host/?accessToken={access_token}')
+    _, cparams = dialect.create_connect_args(u)
+
+    assert cparams['http_scheme'] == HTTPS
+    assert isinstance(cparams['auth'], JWTAuthentication)
+    assert cparams['auth'].token == access_token
